60c54538f19003d8933e9968b8cb64bd5e31f97f
rickiwasho <ricardo.coronado@alumnos.uach.cl>
2021-08-03 12:51:32 -0400

    añadiendo iconos material-ui

--- src/componentes/Contact.js
+++ src/componentes/Contact.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import './Contact.css'
+import CallIcon from '@material-ui/icons/Call';
+import EmailIcon from '@material-ui/icons/Email';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 function Contact() {
     return (
-        <a>a</a>
+        <div className='row'>
+            <div className='column'>
+                <h2>Escríbanos un mensaje </h2>
+                <hr></hr>
+                <p className='text'>
+                   Contáctenos de Lunes a Viernes entre 9:00hrs a 18:30hrs o complete el
+                   formulario con su mensaje y le responderemos a la brevedad.
+                </p>
+                <div className='icon-text'>
+                    <CallIcon></CallIcon>
+                    <h5>+56 5 51369877</h5>
+                </div>
+        
+                <hr></hr>
+
+                <div className='icon-text'>
+                    <EmailIcon></EmailIcon>
+                    <h5>info@gagaga.ad</h5>
+                </div>
+
+                <hr></hr>
+
+                <div className='icon-text'>
+                    <LocationOnIcon></LocationOnIcon>
+                    <h5>Av. Apoquindo 4501 Oficina 1603, Las Condes, Santiago - Chile</h5>
+                </div>
+
+            </div>
+
+            <div className='column'>
+
+            </div>
+        </div>
     )
 }
 
