eaaf32110ffbb7159627b1bee895ab293f57785e
rickiwasho <ricardo.coronado@alumnos.uach.cl>
2021-08-10 17:47:53 -0400

    agregando Leaflet, aunque no funca

--- src/componentes/Contact.js
+++ src/componentes/Contact.js
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import './Contact.css'
 import CallIcon from '@material-ui/icons/Call';
 import EmailIcon from '@material-ui/icons/Email';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 function Contact() {
@@ -18,16 +19,12 @@ function Contact() {
                 <div className='icon-text'>
                     <CallIcon className='icon'></CallIcon>
                     <h5>+56 5 51369877</h5>
-                </div>
-        
-                
+                </div>    
 
                 <div className='icon-text'>
                     <EmailIcon className='icon'></EmailIcon>
                     <h5>info@gagaga.ad</h5>
                 </div>
-
-                
 
                 <div className='icon-text'>
                     <LocationOnIcon className='icon' ></LocationOnIcon>
@@ -38,8 +35,18 @@ function Contact() {
 
             <div className='column'>
                 <h2>Casa Matriz</h2>
-                <div className='map-container'>
-                        <p> acá va un mapa </p>
+                <div id='mapid' className='map-container'>
+                    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
         </div>
